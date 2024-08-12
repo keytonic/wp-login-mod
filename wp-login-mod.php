76052f0cc781681a0eb7bf31177a75de24ad6656
keytonic <20858630+keytonic@users.noreply.github.com>
2024-08-12 15:50:15 -0400

    Update wp-login-mod.php

--- wp-login-mod.php
+++ wp-login-mod.php
@@ -4,7 +4,7 @@
      * wp-login-mod
      *
      * Plugin Name:       wp-login-mod
-     * Description:       This easy-to-use WordPress plugin makes your site require login by sending all visitors to the login page. It turns your site into a private login-only landing page. You can also stop new users from registering, add a message on the login page, and include your own CSS and JS.
+     * Description:       This easy-to-use WordPress plugin forces user login by redirecting all visitors to the login page, effectively turning your site into a private, login-only landing page. You can also prevent new users from registering, add a message on the login page, and include your own CSS and JS.
      * Version:           1.00.0
      * Author:            keytonic
      * Author URI:        https://www.keytonic.net
